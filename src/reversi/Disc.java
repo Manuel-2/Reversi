63443b553003edf7686d99ead62594ea9e7e0513
Manuel Peñaloza <25lopezm25@gmail.com>
2024-10-06 21:36:10 -0700

    ahora el contructor que crea un disco a partir de otro copia la posicion, tambien se le agregaron getters y setters para el color del disco

--- src/reversi/Disc.java
+++ src/reversi/Disc.java
@@ -2,16 +2,33 @@ package reversi;
 
 public class Disc {
 	DiscColors color;
-	
-	public Disc(DiscColors color) {
+	public final Position2D position;
+
+	public Disc(DiscColors color, Position2D position) {
 		this.color = color;
+		this.position = position;
 	}
-	
+
 	/**
 	 * Makes a new Disc as a copy of an already existing Disc
+	 * 
 	 * @param existingDisc the disc you wan't to copy
 	 */
 	public Disc(Disc existingDisc) {
+		if (existingDisc == null) {
+			throw new IllegalArgumentException("Trying to create a new Disc based on a null pointer");
+		}
+		// Note: records are passed by reference, but all of its properties are final, and immutable
+		this.position = existingDisc.position;
 		this.color = existingDisc.color;
+	}
+
+	public DiscColors getColor() {
+		return color;
+	}
+
+	public void setColor(DiscColors color) {
+		// todo: update some static variable that stores the totality of disc
+		this.color = color;
 	}
 }
