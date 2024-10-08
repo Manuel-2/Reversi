2775070e71c3614349bc4d85eed4870da9f6b42a
Manuel Peñaloza <25lopezm25@gmail.com>
2024-10-08 14:27:59 -0700

    el gameBoard coloca las piezas iniciales al ser instanciado

--- src/reversi/GameBoard.java
+++ src/reversi/GameBoard.java
@@ -5,9 +5,22 @@ class GameBoard {
 	private final Disc boardGrid[][];
 
 	public GameBoard(int boardXWidth, int boardYHeight) {
+		if(boardXWidth%2 != 0 && boardYHeight%2 != 0) {
+			throw new IllegalArgumentException("Gameboard dimentions need to be a multiple of 2");
+		}
+		
 		this.boardXWidth = boardXWidth;
 		this.boardYHeight = boardYHeight;
-		boardGrid = new Disc[boardXWidth][boardYHeight];
+		boardGrid = new Disc[boardXWidth-1][boardYHeight-1];
+		
+		DiscColors color = DiscColors.white;
+		for (int x = boardXWidth / 2 - 1; x < boardXWidth / 2 + 1; x++) {
+			for (int y = boardYHeight / 2 - 1; y < boardYHeight; y++) {
+				Position2D position = new Position2D(x, y);
+				this.insertNewDisc(new Disc(color,position), position);
+				color = DiscColors.invert(color);
+			}
+		}
 	}
 
 	public void insertNewDisc(Disc disc, int x, int y) {
@@ -60,8 +73,14 @@ class GameBoard {
 	}
 
 	public boolean isPositionOutsideBoardGrid(Position2D position) {
-		int x = position.x();
-		int y = position.y();
-		return (x >= boardXWidth || x < 0 || y >= boardYHeight || y < 0);
+		return isPositionOutsideBoardGrid(position.x(), position.y());
+	}
+
+	public boolean isPositionInsideBoardGrid(int x, int y) {
+		return !isPositionOutsideBoardGrid(x, y);
+	}
+
+	public boolean isPositionInsideBoardGrid(Position2D position) {
+		return !isPositionOutsideBoardGrid(position);
 	}
 }
